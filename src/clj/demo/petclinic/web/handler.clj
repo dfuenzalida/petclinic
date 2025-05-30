e44099df79f2390a588c905a2421d4c27c05d912
Denis Fuenzalida <denis.fuenzalida@gmail.com>
2025-05-29 23:49:43 -0700

    Fix translations of error pages

--- src/clj/demo/petclinic/web/handler.clj
+++ src/clj/demo/petclinic/web/handler.clj
@@ -1,34 +1,39 @@
 (ns demo.petclinic.web.handler
   (:require
+    [clojure.tools.logging :as log]
     [demo.petclinic.web.middleware.core :as middleware]
     [integrant.core :as ig]
     [ring.util.http-response :as http-response]
     [reitit.ring :as ring]
     [reitit.swagger-ui :as swagger-ui]
-    [demo.petclinic.web.pages.layout :as layout]))
+    [demo.petclinic.web.pages.layout :as layout]
+    [demo.petclinic.web.translations :as tr]))
 
 (defmethod ig/init-key :handler/ring
   [_ {:keys [router api-path] :as opts}]
   (ring/ring-handler
    (router)
-    (ring/routes
-     ;; Handle trailing slash in routes - add it + redirect to it
-     ;; https://github.com/metosin/reitit/blob/master/doc/ring/slash_handler.md
-     (ring/redirect-trailing-slash-handler)
-     (ring/create-resource-handler {:path "/"})
-     (when (some? api-path)
-       (swagger-ui/create-swagger-ui-handler {:path api-path
-                                              :url  (str api-path "/swagger.json")}))
-     (ring/create-default-handler
-      {:not-found
-       (constantly (layout/error-page {:status 404 :title "Not found" :message "The page you tried to visit was not found."}))
+   (ring/routes
+    ;; Handle trailing slash in routes - add it + redirect to it
+    ;; https://github.com/metosin/reitit/blob/master/doc/ring/slash_handler.md
+    (ring/redirect-trailing-slash-handler)
+    (ring/create-resource-handler {:path "/"})
+    (when (some? api-path)
+      (swagger-ui/create-swagger-ui-handler {:path api-path
+                                             :url  (str api-path "/swagger.json")}))
+    (ring/create-default-handler
+     {:not-found
+      (fn [req]
+        (layout/error-page
+         (tr/with-translation {:status 404 :title "Not found" :message "Page not found."} req)))
 
-       :method-not-allowed
-       (constantly (layout/error-page {:status 405 :title "Not allowed"}))
+      :method-not-allowed
+      (fn [req]
+        (layout/error-page (tr/with-translation {:status 405 :title "Not allowed"} req)))
 
-       :not-acceptable
-       (constantly (layout/error-page {:status 406 :title "Not acceptable"}))}))
-    {:middleware [(middleware/wrap-base opts)]}))
+      :not-acceptable
+      (fn [req] (layout/error-page (tr/with-translation {:status 406 :title "Not acceptable"} req)))}))
+   {:middleware [(middleware/wrap-base opts)]}))
 
 (defmethod ig/init-key :router/routes
   [_ {:keys [routes]}]
