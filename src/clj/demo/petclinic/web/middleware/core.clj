9bc22a4138ae9fa274852cdcad608e22248219e9
Denis Fuenzalida <denis.fuenzalida@gmail.com>
2025-06-08 20:54:02 -0700

    Load font-awesome from local webjars

--- src/clj/demo/petclinic/web/middleware/core.clj
+++ src/clj/demo/petclinic/web/middleware/core.clj
@@ -2,13 +2,14 @@
   (:require
     [demo.petclinic.env :as env]
     [ring.middleware.defaults :as defaults]
-    [ring.middleware.session.cookie :as cookie]))
+    [ring.middleware.session.cookie :as cookie]
+    [ring.middleware.webjars :refer [wrap-webjars]]))
 
 (defn wrap-base
   [{:keys [metrics site-defaults-config cookie-secret] :as opts}]
   (let [cookie-store (cookie/cookie-store {:key (.getBytes ^String cookie-secret)})]
     (fn [handler]
       (cond-> ((:middleware env/defaults) handler opts)
-              true (defaults/wrap-defaults
-                     (assoc-in site-defaults-config [:session :store] cookie-store))
-              ))))
+        true (-> (defaults/wrap-defaults
+                  (assoc-in site-defaults-config [:session :store] cookie-store))
+                 wrap-webjars)))))
