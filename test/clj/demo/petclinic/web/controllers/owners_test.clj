7c0b8717f46bf51791caf936b3fa8b0cd5b7211f
Denis Fuenzalida <denis.fuenzalida@gmail.com>
2025-06-15 00:48:18 -0700

    More owners tests

--- test/clj/demo/petclinic/web/controllers/owners_test.clj
+++ test/clj/demo/petclinic/web/controllers/owners_test.clj
@@ -2,8 +2,10 @@
   (:require
    [clojure.test :refer [deftest testing is use-fixtures]]
    [clojure.string :refer [includes?]]
+   [clojure.tools.logging :as log]
+   [ring.middleware.anti-forgery :refer [*anti-forgery-token*]]
    [demo.petclinic.web.pagination :as pagination]
-   [demo.petclinic.test-utils :refer [system-state system-fixture GET]]))
+   [demo.petclinic.test-utils :refer [system-state system-fixture GET POST]]))
 
 (use-fixtures :once (system-fixture))
 
@@ -11,26 +13,66 @@
   ["George Franklin" "Betty Davis" "Eduardo Rodriquez" "Harold Davis" "Peter McTavish"
    "Jean Coleman" "Jeff Black" "Maria Escobito" "David Schroeder" "Carlos Estaban"])
 
-(deftest owners-controller-tests []
+(deftest owners-list-tests []
   (testing "Owners list page 1"
     (let [handler (:handler/ring (system-state))
           params {:lastName ""}
           headers {}
-          response (GET handler "/owners" params headers)]
+          response (GET handler "/owners" params headers)
+          body (:body response)]
       (is (= 200 (:status response)))
-      (is (includes? (:body response) "<a href=\"?page=2\" title=\"Next\" class=\"fa fa-step-forward\"></a>"))
+      (is (includes? body "<a href=\"?page=2\" title=\"Next\" class=\"fa fa-step-forward\"></a>"))
       (dorun
        (for [name (take pagination/PAGESIZE owner-names)]
-         (is (includes? (:body response) (format ">%s</a></td>" name)))))))
+         (is (includes? body (format ">%s</a></td>" name)))))))
 
   (testing "Owners list page 2"
     (let [handler (:handler/ring (system-state))
           params {}
           headers {}
-          response (GET handler "/owners?page=2" params headers)]
+          response (GET handler "/owners?page=2" params headers)
+          body (:body response)]
       (is (= 200 (:status response)))
-      (is (includes? (:body response) "<a href=\"?page=1\" title=\"Previous\" class=\"fa fa-step-backward\"></a>"))
+      (is (includes? body "<a href=\"?page=1\" title=\"Previous\" class=\"fa fa-step-backward\"></a>"))
       (dorun
        (for [name (drop pagination/PAGESIZE owner-names)]
-         (is (includes? (:body response) (format ">%s</a></td>" name))))))))
+         (is (includes? body (format ">%s</a></td>" name)))))))
 
+  (testing "Owners list with known lastName filter"
+    (let [handler (:handler/ring (system-state))
+          params {:lastName "Davis"}
+          headers {}
+          response (GET handler "/owners" params headers)
+          body (:body response)]
+      (is (= 200 (:status response)))
+      (is (includes? body ">Betty Davis</a></td>"))
+      (is (includes? body ">Harold Davis</a></td>"))))
+
+  (testing "Owners list with when lastName filter finds no matches"
+    (let [handler (:handler/ring (system-state))
+          params {:lastName "xyz"}
+          headers {}
+          response (GET handler "/owners" params headers)
+          body (:body response)]
+      (is (= 200 (:status response)))
+      (is (includes? body "has not been found")))))
+
+(deftest owners-creation-tests
+  (testing "Owners creation form"
+    (let [handler (:handler/ring (system-state))
+          params {}
+          headers {}
+          response (GET handler "/owners/new" params headers)
+          body (:body response)]
+      (is (= 200 (:status response)))
+      (is (includes? body "<span>Owner</span>"))
+      (is (includes? body ">First Name</label>"))
+      (is (includes? body ">Last Name</label>"))
+      (is (includes? body ">Address</label>"))
+      (is (includes? body ">City</label>"))
+      (is (includes? body ">Telephone</label>"))
+      ;;
+      ))
+
+  ;;
+  )
