d93d47fab1273e2eb9476b4b6fe21af78511ece4
Denis Fuenzalida <denis.fuenzalida@gmail.com>
2025-06-18 00:11:45 -0700

    Owner creation test

--- test/clj/demo/petclinic/web/controllers/owners_test.clj
+++ test/clj/demo/petclinic/web/controllers/owners_test.clj
@@ -4,7 +4,8 @@
    [clojure.string :refer [includes?]]
    [clojure.tools.logging :as log]
    [demo.petclinic.web.pagination :as pagination]
-   [demo.petclinic.test-utils :refer [system-state system-fixture GET POST get-cookie get-csrf-token]]))
+   [demo.petclinic.test-utils :refer [system-state system-fixture GET POST get-cookie get-csrf-token]]
+   [ring.mock.request :as mock]))   
 
 (use-fixtures :once (system-fixture))
 
@@ -86,5 +87,41 @@
       (is (includes? body "must not be blank"))
       (is (includes? body "Telephone must be a 10-digit number"))))
 
+  (testing "Owners creation form with valid data"
+    (let [handler (:handler/ring (system-state))
+
+          ;; Get the csrf token and cookie from the GET response
+          get-response (GET handler "/owners/new" {} {})
+          token (get-csrf-token get-response)
+          cookie (get-cookie get-response)
+          base64 (.withoutPadding (java.util.Base64/getEncoder))
+
+          ;; Generate a random last name for the owner
+          last-name (->> (random-uuid) str .getBytes (.encodeToString base64) (take 10) (apply str))
+          params {:__anti-forgery-token token
+                  :first_name "John"
+                  :last_name last-name
+                  :address "123 Elm St"
+                  :city "Springfield"
+                  :telephone "1234567890"}
+
+          ;; Send the POST request to create the owner
+          response (-> (mock/request :post "/owners/new" params)
+                       (mock/header "x-csrf-token" token)
+                       (mock/header "Cookie" cookie)
+                       handler)]
+      (is (= 302 (:status response)))
+      (is (includes? (:headers response) "Location"))
+      (is (includes? (:headers response) "/owners"))
+
+      ;; Finally, check if the owner was created in the DB
+      (let [query-fn (:db.sql/query-fn (system-state))
+            params {:lastNameLike last-name :pagesize 1 :page 1}
+            owner  (-> (query-fn :get-owners params) first)]
+        (is (= "John" (:first_name owner)))
+        (is (= last-name (:last_name owner)))
+        (is (= "123 Elm St" (:address owner)))
+        (is (= "Springfield" (:city owner)))
+        (is (= "1234567890" (:telephone owner))))))
   ;;
   )
